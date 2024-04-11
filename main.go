6e2dcf6fcbea3a897f2670c420f2fa46cc2e78fb
fazriegi <fazriegi80@gmail.com>
2024-04-11 11:23:07 +0700

    update cli calculator

--- main.go
+++ main.go
@@ -1,53 +1,210 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
 )
 
+type History struct {
+	firstOperand  float64
+	secondOperand float64
+	operator      string
+	result        float64
+}
+
+var histories []History
+
 func main() {
-	errUsageResponse := "Usage: go run main.go [first_operand] [operator] [second_operand]"
+	fmt.Println("========== CLI Calculator ==========")
+	menu()
+}
 
-	args := os.Args
+func menu() {
+	for {
+		fmt.Println("\n========== Menu ==========")
+		fmt.Println("Menu:")
+		fmt.Println("1. Calculator")
+		fmt.Println("2. History")
+		fmt.Println("3. Exit")
+		fmt.Print("What you want to do? : ")
 
-	if len(args) != 4 {
-		fmt.Println(errUsageResponse)
-		os.Exit(1)
-	}
+		var inputMenu string
+		fmt.Scan(&inputMenu)
 
-	firstOperand, err := strconv.ParseFloat(args[1], 64)
-	if err != nil {
-		fmt.Println("Operands must be numeric")
-		fmt.Println(errUsageResponse)
-		os.Exit(1)
-	}
-
-	secondOperand, err := strconv.ParseFloat(args[3], 64)
-	if err != nil {
-		fmt.Println("Operands must be numeric")
-		fmt.Println(errUsageResponse)
-		os.Exit(1)
-	}
-
-	operator := args[2]
-
-	switch operator {
-	case "+":
-		fmt.Println("Result:", firstOperand+secondOperand)
-	case "-":
-		fmt.Println("Result:", firstOperand-secondOperand)
-	case "/":
-		if firstOperand == 0 || secondOperand == 0 {
-			fmt.Println("Can not divide by zero")
-			os.Exit(1)
+		switch inputMenu {
+		case "1":
+			calculator()
+		case "2":
+			showHistory()
+		case "3":
+			fmt.Println("\nGood bye!")
+			os.Exit(0)
+		default:
+			fmt.Println("Please enter the correct number of menu")
 		}
-		fmt.Println("Result:", firstOperand/secondOperand)
-	case "x":
-		fmt.Println("Result:", firstOperand*secondOperand)
-	default:
-		fmt.Println(errUsageResponse)
-		fmt.Println("Supported operators: + - / x")
-		os.Exit(1)
 	}
+}
+
+func showHistory() {
+	fmt.Println("\n========== History ==========")
+	for _, history := range histories {
+		fmt.Println(history.firstOperand, history.operator, history.secondOperand, "=", history.result)
+	}
+}
+
+func calculator() {
+	fmt.Println("\n========== Calculator ==========")
+
+	var (
+		operators     = []string{"+", "-", "/", "*", "="}
+		inputOperand  string
+		operator      string
+		firstOperand  float64
+		secondOperand float64
+		err           error
+	)
+
+	fmt.Print(`Operators:
++ : for additions
+- : for subtractions
+/ : for divisions
+* : for multiplications
+= : to return to the menu
+`)
+
+	for {
+		fmt.Print("\nEnter the operand: ")
+		fmt.Scan(&inputOperand)
+
+		firstOperand, err = strconv.ParseFloat(inputOperand, 64)
+		if err != nil {
+			fmt.Println("Please enter a numeric operand")
+			continue
+		} else {
+			break
+		}
+	}
+
+	for {
+		fmt.Print("Enter the operator: ")
+		fmt.Scan(&operator)
+		if operator == "=" {
+			return
+		} else if !isOperatorExist(operator, operators) {
+			fmt.Println("\nPlease enter the correct operator")
+			continue
+		}
+
+		fmt.Print("Enter the operand: ")
+		fmt.Scan(&inputOperand)
+
+		secondOperand, err = strconv.ParseFloat(inputOperand, 64)
+		if err != nil {
+			fmt.Println("Please enter a numeric operand")
+			continue
+		}
+
+		switch operator {
+		case "+":
+			result := addition(firstOperand, secondOperand)
+
+			history := History{
+				firstOperand,
+				secondOperand,
+				operator,
+				result,
+			}
+			histories = append(histories, history)
+
+			firstOperand = result
+			fmt.Println("== Result:", result, " ==")
+			fmt.Println("")
+
+		case "-":
+			result := subtraction(firstOperand, secondOperand)
+
+			history := History{
+				firstOperand,
+				secondOperand,
+				operator,
+				result,
+			}
+			histories = append(histories, history)
+
+			firstOperand = result
+			fmt.Println("== Result:", result, " ==")
+			fmt.Println("")
+
+		case "/":
+			result, err := division(firstOperand, secondOperand)
+
+			if err != nil {
+				fmt.Println(err)
+				fmt.Println("")
+			} else {
+				history := History{
+					firstOperand,
+					secondOperand,
+					operator,
+					result,
+				}
+				histories = append(histories, history)
+
+				firstOperand = result
+				fmt.Println("== Result:", result, " ==")
+				fmt.Println("")
+			}
+
+		case "*":
+			result := multiplication(firstOperand, secondOperand)
+
+			history := History{
+				firstOperand,
+				secondOperand,
+				operator,
+				result,
+			}
+			histories = append(histories, history)
+
+			firstOperand = result
+			fmt.Println("== Result:", result, " ==")
+			fmt.Println("")
+
+		default:
+			fmt.Println("Please enter the correct operator")
+			fmt.Println("")
+		}
+	}
+}
+
+func isOperatorExist(operator string, operators []string) bool {
+	for i := range operators {
+		if operator == operators[i] {
+			return true
+		}
+	}
+
+	return false
+}
+
+func addition(firstOperand, secondOperand float64) float64 {
+	return firstOperand + secondOperand
+}
+
+func subtraction(firstOperand, secondOperand float64) float64 {
+	return firstOperand - secondOperand
+}
+
+func division(firstOperand, secondOperand float64) (float64, error) {
+	if firstOperand == 0 || secondOperand == 0 {
+		return 0, errors.New("Can not divide by zero")
+	}
+
+	return firstOperand / secondOperand, nil
+}
+
+func multiplication(firstOperand, secondOperand float64) float64 {
+	return firstOperand * secondOperand
 }
